d29baa803d4226fd3c97bf7c75e52036ebed8aa2
GonzaLenardon <gonlenardon@gmail.com>
2023-07-28 09:14:50 -0300

    view actors added to movie details

--- src/common/ActorCard.jsx
+++ src/common/ActorCard.jsx
@@ -1,7 +1,7 @@
 export const ActorCard = ({ actor }) => {
   return (
-    <div className="col-6 col-sm-2 my-3 text-center">
-      <p>{actor.name} </p>
+    <div className="col-6 col-sm-2 my-3 text-center fs-5 ">
+      <p className="text-white fw-medium">{actor.name} </p>
       <img
         src={`https://image.tmdb.org/t/p/w200/${actor.profile_path}`}
         className="rounded-4 img-fluid"
---
+++ src/common/Navigation.jsx
@@ -0,0 +1,14 @@
+export const Navigation = () => {
+  return (
+    <div className="text-center">
+      <button type="button" className="btn btn-outline-info">
+        Prev
+      </button>
+      <label>numero</label>
+
+      <button type="button" className="btn btn-outline-info">
+        Next
+      </button>
+    </div>
+  );
+};
